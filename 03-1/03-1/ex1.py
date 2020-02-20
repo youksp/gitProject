cc1255df7e304a6c1a159daa06c00f6e35465bfa
remove <youksp@gmail.com>
2020-02-20 11:16:36 +0900

    minval 증가

--- 03-1/03-1/ex1.py
+++ 03-1/03-1/ex1.py
@@ -1,7 +1,7 @@
 data = input("Enter list of numbers: ")
 numbers = data.split()
 numbers = [int(i) for i in numbers]
-minval = 99999999
+minval = 9999999999999
 for val in numbers:
     if (minval > val):
         minval = val
