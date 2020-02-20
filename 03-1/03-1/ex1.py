cfd626775621669a6906a6664aebc04c53341dd7
remove <youksp@gmail.com>
2020-02-20 14:15:29 +0900

    branch

--- 03-1/03-1/ex1.py
+++ 03-1/03-1/ex1.py
@@ -2,6 +2,8 @@ data = input("Enter list of numbers: ")
 numbers = data.split()
 numbers = [int(i) for i in numbers]
 minval = 9999999999998 #gitProject
+
+master = 0;
 for val in numbers: 
     if (minval > val):
         minval = val
